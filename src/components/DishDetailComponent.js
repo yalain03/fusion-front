29ecb949d14bc0c81c62fa097913577b7596dac2
Yves <yalain9@gmail.com>
2018-10-22 14:04:35 -0500

    Fetch command

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -4,6 +4,7 @@ import { Card, CardImg, CardText, CardBody, Button, Breadcrumb, BreadcrumbItem,
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import { Link } from 'react-router-dom';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
 
 const required = (val) => val && val.length;
 const minLength = (len) => (val) => !val || (val.length >= len);
@@ -14,7 +15,7 @@ function RenderDish({dish}) {
         <div>
             <Card>
                 <CardBody>
-                    <CardImg top src={dish.image} alt={dish.name} />
+                    <CardImg top src={baseUrl + dish.image} alt={dish.name} />
                     <CardText>{dish.description}</CardText>
                 </CardBody>
             </Card>
--- src/redux/leaders.js
+++ src/redux/leaders.js
@@ -1,7 +1,16 @@
-import { LEADERS } from '../shared/leaders';
+import * as ActionTypes from './ActionTypes';
 
-export const Leaders = (state = LEADERS, action) => {
+export const Leaders = (state = { errMess: null, leaders:[]}, action) => {
     switch (action.type) {
+        case ActionTypes.ADD_LEADERS:
+            return {...state, errMess: null, leaders: action.payload};
+
+        case ActionTypes.LEADERS_FAILED:
+            return {...state, errMess: action.payload};
+
+        case ActionTypes.LEADERS_LOADING:
+            return {...state, isLoading: true, errMess: null, leaders: []}
+
         default:
             return state;
     }
